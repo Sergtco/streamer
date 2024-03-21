b3fa021afd99edc966c99a046b5381c1bc12c0a8
sergtco <sergtcorge@gmail.com>
2024-03-21 15:14:43 +0300

    Minor changes

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -6,14 +6,15 @@ import (
 	"stream/pkg"
 )
 
-
 func main() {
+	addr, port := "127.0.0.1", "8080"
 	http.HandleFunc("/get", pkg.ServeSong)
-    http.HandleFunc("/fetch", pkg.FetchDB)
+	http.HandleFunc("/fetch", pkg.FetchDB)
 	http.HandleFunc("/segments/", pkg.ServeTS)
-    http.HandleFunc("/getSongData", pkg.GetSongData)
-    http.HandleFunc("/deleteSong", pkg.DeleteHandler)
-	if http.ListenAndServe(":8080", nil) == nil {
+	http.HandleFunc("/getSongData", pkg.GetSongData)
+	http.HandleFunc("/deleteSong", pkg.DeleteHandler)
+	fmt.Printf("Listening on %s:%s", addr, port)
+	if http.ListenAndServe(fmt.Sprintf("%s:%s", addr, port), nil) == nil {
 		fmt.Println("Exited")
 	}
 }
